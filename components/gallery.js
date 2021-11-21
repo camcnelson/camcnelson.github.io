1dca2cadd424b62b82707ba3bb3a24de1919318e
camcnelson <cameroncaseynelson@gmail.com>
2021-11-20 20:14:07 -0500

    major style edits, content

---
+++ components/gallery.js
@@ -0,0 +1,57 @@
+class Gallery extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+
+        var src = this.getAttribute('src');
+        var title = this.getAttribute('title');
+        var text = this.getAttribute('text');
+
+        $(document).ready(function() {
+
+            $.ajax({
+                url : src,
+                success: function (data) {
+
+                    if((title != null) || (description != null)) {
+                        var title_card = `
+                        <div class='card'>
+                            <div class='card-body' style='text-align:justify'>`
+                            + (title!=null ? `<div class='card-title'>` + title + `</div>`:'')
+                            + (text!=null ? `<div class='card-text'>` + text + `</div>`:'')
+                            + `</div>
+                        `;
+                        $("#gallery_header").append( title_card );
+                    }
+
+                    $(data).find("a").attr("href", function (i, val) {
+                        if( val.match(/\.(jpe?g|png|gif)$/) ) {
+                            var card = `
+                            <div class='card'>
+                            <a data-fancybox='gallery' href='`+ val + `'>
+                                <img class='card-img-top' src='`+ val + `'/>
+                            </a>
+                            </div>
+                            `;
+                            $("#gallery").append( card );
+                        }
+                    });
+                }
+            });
+            
+        });
+
+        this.innerHTML = `
+        <div class='card-columns' id='gallery_header' style='column-count:1;'></div>
+        <div class='card-columns'>
+        <div id='gallery'></div>
+        </div>
+        `;
+
+    }
+  }
+  
+  customElements.define('gallery-component', Gallery);
+  
--- components/header.js
+++ components/header.js
@@ -5,32 +5,15 @@ class Header extends HTMLElement {
   
     connectedCallback() {
 
-      // this.innerHTML = `
-      //   <nav class="navbar navbar-expand-md navbar-light">
-      //       <button class="navbar-toggler rounded-0" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      //           <span class="navbar-toggler-icon"></span>
-      //       </button>
-      //       <div class="collapse navbar-collapse" id="navbarNav" style="background-color: white;">
-      //           <ul class="navbar-nav">
-      //               <li>
-      //                   <a class="nav-link" href="#" style="color: black;">Home</a>
-      //               </li>
-      //               <li class="nav-item">
-      //                   <a class="nav-link" href="#" style="color: black;">About</a>
-      //               </li>
-      //           </ul>
-      //       </div>
-      //   </nav>
-      // `;
-
         this.innerHTML = `
         <div class='header'>
         <ul class='menubar'>
             <li><a href='/index.html'><h1 style='color:black'>Home</h1></a></li>
             <li><a href='/pages/about.html'>About</a></li>
-            <li><a href='mailto:cn339@cornell.edu'>Contact</a></li>
+            <li><a href='https://linktr.ee/camnelson'>More</a></li>
         </ul>
         </div>
+        <div class='header'><p style='margin:1rem;'>Cameron Nelson</p></div>
         `;
 
     }
