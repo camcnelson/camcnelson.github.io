7453faf734bf879184221ba502b172d84f00613f
camcnelson <cameroncaseynelson@gmail.com>
2022-03-11 19:10:45 -0500

    added rust

--- jocms/Installer/inc.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once('../core/inc/settings.php');
-$JO_LANG = array_merge($JO_LANG,jo_language($JO_S['lang'],"/jocms/Installer/"));
- ?>
--- jocms/uploads/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-header('Location: /jocms/control/cms.php');
-?>
